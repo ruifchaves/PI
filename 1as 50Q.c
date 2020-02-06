e90a093b795e18d9040c495e5eabe91026ef8fd3
Rui <ruichaves99@gmail.com>
2020-02-06 23:28:30 +0000

    Questões 2 e 3 adicionadas

--- 1as 50Q.c
+++ 1as 50Q.c
@@ -18,7 +18,38 @@ void maior(){
 	printf("O maior número é o %d.\n", maior);
 }
 
+void media(){
+	int soma=0, i=0, x;
+	float media=0;
+	printf("Escreva uma sequência de números inteiros e termine com 0.\n");
+	do{
+		scanf("%d",&x);
+		i+=1;
+		soma=soma+x;
+		//printf("soma: %d, i: %d\n",soma,i); 				para teste
+	}while(x!=0);
+	i--;													//o i incrimenta com x = 0 daí decrementar uma vez fora dps do ciclo
+	media = soma/i;
+	printf("A média dos números é %.5f.\n", media);         //%.5f porque a media corresponde a um float, com 5 digitos dps do ponto
+}
+
+void sndmaior(){
+	int maior=0, segundomaior=0, x;
+	printf("Escreva uma sequência de números inteiros e termine com 0.\n");
+	do{
+		scanf("%d",&x);
+		if(x > maior){
+			segundomaior = maior;
+			maior = x;
+		}
+		else
+			if(x > segundomaior) segundomaior=x;
+	} while(x!=0);
+	printf("O segundo maior número é o %d.\n", segundomaior);
+}
 
 void main(){
-	maior();
+	//maior();
+	//media();
+	sndmaior();
 }
