36fa5a5c17dfff60759af9945b88fe5f514d2f2b
Rui <ruichaves99@gmail.com>
2020-02-17 13:11:38 +0000

    strcmp

--- 1as 50Q.c
+++ 1as 50Q.c
@@ -141,3 +141,9 @@ char *mystrcpy (char *dest, char source[]){
 	dest[i]='\0';
 	return dest;
 }
+
+int mystrcmp(char s1[], char s2[]){
+	int i=0;
+	while(s1[i] && s2[i] && s1[i]==s2[i]) i++;
+	return (s1[i]-s2[i]);
+}
